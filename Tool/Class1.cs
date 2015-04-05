c784f04b7d4fad20b240c379b3d79c1a2c50f93c
1101137106 <1101137106@gm.kuas.edu.tw>
2015-04-05 18:05:12 +0800

    final HW

---
+++ Tool/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tool
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void CalBMI()
+        {
+            int a = 2;
+            int b = 1;
+            Assert.AreEqual(a, b);
+        }
+    }
+}
---
+++ test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            double BMI, Value = 0;
+            BMI = float.Parse(txtWeight.Text) / ((float.Parse(txtHeight.Text) / 100) * (float.Parse(txtHeight.Text) / 100));
+            Value = Math.Round(BMI, 3);
+            Debug.WriteLine(Value);
+        }
+    }
+}
